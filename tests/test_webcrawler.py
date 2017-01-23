f606979f4962d0d29e725d8cc21e26d9424bcf27
Lucas Moura <lucas.moura128@gmail.com>
2017-01-23 15:14:53 -0200

    Add tests to check for movie festivals paragraph

--- tests/test_webcrawler.py
+++ tests/test_webcrawler.py
@@ -26,3 +26,26 @@ class WebCrawlerTest(unittest.TestCase):
         expected = 15
         self.assertEqual(mrd.get_date_index_from_movie_review(movie_review),
                          expected)
+
+    def test_movie_festival_paragraph(self):
+        with open('tests/files/movie_review_2.txt', 'r') as movie_text:
+            movie_review = BeautifulSoup(movie_text, 'html.parser')
+
+        movie_review = movie_review.find(
+            'div', {'class': 'critica-conteudo'})
+
+        expected = True
+        date_index = mrd.get_date_index_from_movie_review(movie_review)
+        self.assertEqual(mrd.check_for_critics_published_in_movie_festivals(
+            movie_review, date_index - 2), expected)
+
+        with open('tests/files/movie_review_1.txt', 'r') as movie_text:
+            movie_review = BeautifulSoup(movie_text, 'html.parser')
+
+        movie_review = movie_review.find(
+            'div', {'class': 'critica-conteudo'})
+
+        expected = False
+        date_index = mrd.get_date_index_from_movie_review(movie_review)
+        self.assertEqual(mrd.check_for_critics_published_in_movie_festivals(
+            movie_review, date_index - 2), expected)
