88ae7dae9e8b4b99ebca1360647f668b78890489
Lucas Moura <lucas.moura128@gmail.com>
2017-01-23 16:22:06 -0200

    Add test for additional info method

--- tests/test_webcrawler.py
+++ tests/test_webcrawler.py
@@ -49,3 +49,37 @@ class WebCrawlerTest(unittest.TestCase):
         date_index = mrd.get_date_index_from_movie_review(movie_review)
         self.assertEqual(mrd.check_for_critics_published_in_movie_festivals(
             movie_review, date_index - 2), expected)
+
+    def test_movie_additional_info(self):
+        with open('tests/files/movie_review_2.txt', 'r') as movie_text:
+            movie_review = BeautifulSoup(movie_text, 'html.parser')
+
+        movie_review = movie_review.find(
+            'div', {'class': 'critica-conteudo'})
+
+        expected = False
+        date_index = mrd.get_date_index_from_movie_review(movie_review)
+        self.assertEqual(mrd.check_for_observation_in_movie_review(
+            movie_review, date_index - 2), expected)
+
+        with open('tests/files/movie_review_3.txt', 'r') as movie_text:
+            movie_review = BeautifulSoup(movie_text, 'html.parser')
+
+        movie_review = movie_review.find(
+            'div', {'class': 'critica-conteudo'})
+
+        expected = True
+        date_index = mrd.get_date_index_from_movie_review(movie_review)
+        self.assertEqual(mrd.check_for_observation_in_movie_review(
+            movie_review, date_index - 1), expected)
+
+        with open('tests/files/movie_review_4.txt', 'r') as movie_text:
+            movie_review = BeautifulSoup(movie_text, 'html.parser')
+
+        movie_review = movie_review.find(
+            'div', {'class': 'critica-conteudo'})
+
+        expected = True
+        date_index = mrd.get_date_index_from_movie_review(movie_review)
+        self.assertEqual(mrd.check_for_observation_in_movie_review(
+            movie_review, date_index - 1), expected)
