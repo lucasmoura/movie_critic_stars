48f3cbd1f424be4ca96f90038fa89ee0e3b46052
Lucas Moura <lucas.moura128@gmail.com>
2018-02-11 02:32:54 -0200

    Create scripts to run CNN model

---
+++ cnn_model.py
@@ -0,0 +1,173 @@
+import argparse
+import os
+
+import tensorflow as tf
+
+from model.estimator import get_estimator, run_estimator
+from preprocessing.dataset import load
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+
+def get_embedding(embedding_path):
+    return load(embedding_path)
+
+
+def create_argument_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-tf',
+                        '--train-file',
+                        type=str,
+                        help='The path of the train tfrecord file',
+                        required=True)
+
+    parser.add_argument('-vf',
+                        '--validation-file',
+                        type=str,
+                        help='The path of the validation tfrecord file',
+                        required=True)
+
+    parser.add_argument('-tsf',
+                        '--test-file',
+                        type=str,
+                        help='The path of the test tfrecord file',
+                        required=True)
+
+    parser.add_argument('-sp',
+                        '--save-path',
+                        type=str,
+                        help='The path to save the model results',
+                        required=True)
+
+    parser.add_argument('-em',
+                        '--embedding-path',
+                        type=str,
+                        help='The path of the embedding file to be used',
+                        required=True)
+
+    parser.add_argument('-es',
+                        '--embed-size',
+                        type=int,
+                        help='The embedding size',
+                        required=True)
+
+    parser.add_argument('-nl',
+                        '--num-labels',
+                        type=int,
+                        help='The number of labels that a review can be classified')
+
+    parser.add_argument('-nf',
+                        '--num-filters',
+                        type=int,
+                        help='number of filters to use on the convolutional layer')
+
+    parser.add_argument('-fs',
+                        '--filters-size',
+                        type=str,
+                        help='A comma separated string containing the filters size to use')
+
+    parser.add_argument('-wd',
+                        '--weight-decay',
+                        type=float,
+                        help='Weight decay variable for l2 loss')
+
+    parser.add_argument('-emd',
+                        '--embedding-dropout',
+                        type=float,
+                        help='Embedding dropout value')
+
+    parser.add_argument('-dr',
+                        '--dropout-rate',
+                        type=float,
+                        help='Dropout value')
+
+    parser.add_argument('-lr',
+                        '--learning-rate',
+                        type=float,
+                        help='Learning rate to be used when training the model')
+
+    parser.add_argument('-bs',
+                        '--batch-size',
+                        type=int,
+                        help='The batch size to be used')
+
+    parser.add_argument('-ne',
+                        '--num-epochs',
+                        type=int,
+                        help='The number of epochs to train the model')
+
+    parser.add_argument('-bw',
+                        '--bucket-width',
+                        type=int,
+                        help='The bucket width allowed')
+
+    parser.add_argument('-nb',
+                        '--num-buckets',
+                        type=int,
+                        help='The maximum number of buckets')
+    return parser
+
+
+def main():
+    parser = create_argument_parser()
+    user_args = vars(parser.parse_args())
+
+    train_file = user_args['train_file']
+    validation_file = user_args['validation_file']
+    test_file = user_args['test_file']
+    batch_size = user_args['batch_size']
+    bucket_width = user_args['bucket_width']
+    num_buckets = user_args['num_buckets']
+
+    pipeline_params = {
+        'train_file': train_file,
+        'validation_file': validation_file,
+        'test_file': test_file,
+        'batch_size': batch_size,
+        'bucket_width': bucket_width,
+        'num_buckets': num_buckets
+    }
+
+    embed_size = user_args['embed_size']
+    embedding_path = user_args['embedding_path']
+    num_labels = user_args['num_labels']
+    num_filters = user_args['num_filters']
+
+    filters_size = user_args['filters_size']
+    filters_size = [int(value) for value in filters_size.split(',')]
+
+    weight_decay = user_args['weight_decay']
+    embedding_dropout = user_args['embedding_dropout']
+    dropout_rate = user_args['dropout_rate']
+    lr = user_args['learning_rate']
+    show_loss = False
+
+    if show_loss:
+        tf.logging.set_verbosity(tf.logging.INFO)
+
+    matrix = get_embedding(embedding_path)
+    model_params = {
+        'embedding': matrix,
+        'embed_size': embed_size,
+        'vocab_size': len(matrix),
+        'num_labels': num_labels,
+        'num_filters': num_filters,
+        'filters_size': filters_size,
+        'weight_decay': weight_decay,
+        'embedding_dropout': embedding_dropout,
+        'dropout_rate': dropout_rate,
+        'lr': lr,
+        'show_loss': show_loss
+    }
+
+    num_epochs = user_args['num_epochs']
+    save_path = user_args['save_path']
+
+    estimator = get_estimator('cnn')
+    run_estimator(estimator, model_params, pipeline_params, num_epochs, save_path)
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/run_cnn_model.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+set -e
+
+#usage
+#./script/run_cnn_model.sh
+
+FULL_TRAIN_FILE="data/cnn/full/train.tfrecord"
+FULL_VALIDATION_FILE="data/cnn/full/validation.tfrecord"
+FULL_TEST_FILE="data/cnn/full/test.tfrecord"
+FULL_SAVE_PATH="data/cnn/full/model/"
+
+FULL_UNDERSAMPLING_TRAIN_FILE="data/cnn/full/undersampling/train.tfrecord"
+FULL_UNDERSAMPLING_VALIDATION_FILE="data/cnn/full/undersampling/validation.tfrecord"
+FULL_UNDERSAMPLING_TEST_FILE="data/cnn/full/undersampling/test.tfrecord"
+FULL_UNDERSAMPLING_SAVE_PATH="data/cnn/full/undersampling/model/"
+
+OMELETE_TRAIN_FILE="data/cnn/omelete/train.tfrecord"
+OMELETE_VALIDATION_FILE="data/cnn/omelete/validation.tfrecord"
+OMELETE_TEST_FILE="data/cnn/omelete/test.tfrecord"
+OMELETE_SAVE_PATH="data/cnn/omelete/model/"
+
+CEC_TRAIN_FILE="data/cnn/cec/train.tfrecord"
+CEC_VALIDATION_FILE="data/cnn/cec/validation.tfrecord"
+CEC_TEST_FILE="data/cnn/cec/test.tfrecord"
+CEC_SAVE_PATH="data/cnn/cec/model/"
+
+CINECLICK_TRAIN_FILE="data/cnn/cineclick/train.tfrecord"
+CINECLICK_VALIDATION_FILE="data/cnn/cineclick/validation.tfrecord"
+CINECLICK_TEST_FILE="data/cnn/cineclick/test.tfrecord"
+CINECLICK_SAVE_PATH="data/cnn/cineclick/model/"
+
+FULL_EMBEDDING_PATH="data/cnn/full/fasttext.pkl"
+FULL_UNDERSAMPLING_EMBEDDING_PATH="data/cnn/full/undersampling/fasttext.pkl"
+OMELETE_EMBEDDING_PATH="data/cnn/omelete/fasttext.pkl"
+CEC_EMBEDDING_PATH="data/cnn/cec/fasttext.pkl"
+CINECLICK_EMBEDDING_PATH="data/cnn/cineclick/fasttext.pkl"
+
+EMBEDDING_SIZE=300
+NUM_LABELS=5
+NUM_FILTERS=128
+FILTERS_SIZE="2,3,4"
+WEIGHT_DECAY=0.0001
+LEARNING_RATE=0.001
+BATCH_SIZE=32
+NUM_EPOCHS=15
+BUCKET_WIDTH=32
+NUM_BUCKETS=50
+EMBEDDING_DROPOUT=0.5
+DROPOUT_RATE=0.5
+
+echo "Run full model without undersampling"
+python cnn_model.py \
+    --train-file=${FULL_TRAIN_FILE} \
+    --validation-file=${FULL_VALIDATION_FILE} \
+    --test-file=${FULL_TEST_FILE} \
+    --save-path=${FULL_SAVE_PATH} \
+    --embedding-path=${FULL_EMBEDDING_PATH} \
+    --embed-size=${EMBEDDING_SIZE} \
+    --num-labels=${NUM_LABELS} \
+    --num-filters=${NUM_FILTERS} \
+    --filters-size=${FILTERS_SIZE} \
+    --weight-decay=${WEIGHT_DECAY} \
+    --embedding-dropout=${EMBEDDING_DROPOUT} \
+    --dropout-rate=${DROPOUT_RATE} \
+    --learning-rate=${LEARNING_RATE} \
+    --batch-size=${BATCH_SIZE} \
+    --num-epochs=${NUM_EPOCHS} \
+    --bucket-width=${BUCKET_WIDTH} \
+    --num-buckets=${NUM_BUCKETS}
+
+echo "Run full model with undersampling"
+python cnn_model.py \
+    --train-file=${FULL_UNDERSAMPLING_TRAIN_FILE} \
+    --validation-file=${FULL_UNDERSAMPLING_VALIDATION_FILE} \
+    --test-file=${FULL_UNDERSAMPLING_TEST_FILE} \
+    --save-path=${FULL_UNDERSAMPLING_SAVE_PATH} \
+    --embedding-path=${FULL_UNDERSAMPLING_EMBEDDING_PATH} \
+    --embed-size=${EMBEDDING_SIZE} \
+    --num-labels=${NUM_LABELS} \
+    --num-filters=${NUM_FILTERS} \
+    --filters-size=${FILTERS_SIZE} \
+    --weight-decay=${WEIGHT_DECAY} \
+    --embedding-dropout=${EMBEDDING_DROPOUT} \
+    --dropout-rate=${DROPOUT_RATE} \
+    --learning-rate=${LEARNING_RATE} \
+    --batch-size=${BATCH_SIZE} \
+    --num-epochs=${NUM_EPOCHS} \
+    --bucket-width=${BUCKET_WIDTH} \
+    --num-buckets=${NUM_BUCKETS}
+
+echo "Run omelete model"
+python cnn_model.py \
+    --train-file=${OMELETE_TRAIN_FILE} \
+    --validation-file=${OMELETE_VALIDATION_FILE} \
+    --test-file=${OMELETE_TEST_FILE} \
+    --save-path=${OMELETE_SAVE_PATH} \
+    --embedding-path=${OMELETE_EMBEDDING_PATH} \
+    --embed-size=${EMBEDDING_SIZE} \
+    --num-labels=${NUM_LABELS} \
+    --num-filters=${NUM_FILTERS} \
+    --filters-size=${FILTERS_SIZE} \
+    --weight-decay=${WEIGHT_DECAY} \
+    --embedding-dropout=${EMBEDDING_DROPOUT} \
+    --dropout-rate=${DROPOUT_RATE} \
+    --learning-rate=${LEARNING_RATE} \
+    --batch-size=${BATCH_SIZE} \
+    --num-epochs=${NUM_EPOCHS} \
+    --bucket-width=${BUCKET_WIDTH} \
+    --num-buckets=${NUM_BUCKETS}
+
+echo "Run cineclick model"
+python cnn_model.py \
+    --train-file=${CINECLICK_TRAIN_FILE} \
+    --validation-file=${CINECLICK_VALIDATION_FILE} \
+    --test-file=${CINECLICK_TEST_FILE} \
+    --save-path=${CINECLICK_SAVE_PATH} \
+    --embedding-path=${CINECLICK_EMBEDDING_PATH} \
+    --embed-size=${EMBEDDING_SIZE} \
+    --num-labels=${NUM_LABELS} \
+    --num-filters=${NUM_FILTERS} \
+    --filters-size=${FILTERS_SIZE} \
+    --weight-decay=${WEIGHT_DECAY} \
+    --embedding-dropout=${EMBEDDING_DROPOUT} \
+    --dropout-rate=${DROPOUT_RATE} \
+    --learning-rate=${LEARNING_RATE} \
+    --batch-size=${BATCH_SIZE} \
+    --num-epochs=${NUM_EPOCHS} \
+    --bucket-width=${BUCKET_WIDTH} \
+    --num-buckets=${NUM_BUCKETS}
+
+echo "Run Cinema em Cena model"
+python cnn_model.py \
+    --train-file=${CEC_TRAIN_FILE} \
+    --validation-file=${CEC_VALIDATION_FILE} \
+    --test-file=${CEC_TEST_FILE} \
+    --save-path=${CEC_SAVE_PATH} \
+    --embedding-path=${CEC_EMBEDDING_PATH} \
+    --embed-size=${EMBEDDING_SIZE} \
+    --num-labels=${NUM_LABELS} \
+    --num-filters=${NUM_FILTERS} \
+    --filters-size=${FILTERS_SIZE} \
+    --weight-decay=${WEIGHT_DECAY} \
+    --embedding-dropout=${EMBEDDING_DROPOUT} \
+    --dropout-rate=${DROPOUT_RATE} \
+    --learning-rate=${LEARNING_RATE} \
+    --batch-size=${BATCH_SIZE} \
+    --num-epochs=${NUM_EPOCHS} \
+    --bucket-width=${BUCKET_WIDTH} \
+    --num-buckets=${NUM_BUCKETS}
